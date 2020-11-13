bcec860f9c5dc6dffda8f7fb38960c02fd1de44c
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-11-13 12:26:04 -0300

    Keep hydrated! -  Challenge finished.

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ Will he survive?
 Return "true" if yes, "false" otherwise.
 */
 
-/* LOGICAL REASONING
+/* LOGICAL REASONING:
     > Multiply the dragons' quantity by 2.
     > Comparate it with quantity of bullets. 
     > Verify if the quantity of bullets are equal or greater than the quantity of dragons. 
@@ -34,3 +34,34 @@ function hero(bullets, dragons){
   }
 hero(14,7);
 console.log(hero(14,7));
+
+
+/* 2. Keep hydrated!
+_________________________________________________________________
+Because Joe knows it is important to stay hydrated, 
+he drinks 0.5 litres of water per hour of cycling.
+
+You get given the time in hours and you need to return the number of litres Joe will drink, 
+rounded to the smallest value.
+
+For example:
+
+time = 3 -> litres = 1
+
+time = 6.7 -> litres = 3
+
+time = 11.8 -> litres = 5
+*/
+
+/* LOGICAL REASONING:
+    > If 1 hour equals 0.5l, then 1h x 0.5l.
+    > Round the number with the lowest value (if it is a fraction) and return.
+
+It's possible to solve that calculation with a function and the Math.floor method to
+round the to the smallest value:
+*/
+function litres(time) {
+    return (Math.floor(time * 0.5))
+  }
+
+console.log(litres(4.5));
