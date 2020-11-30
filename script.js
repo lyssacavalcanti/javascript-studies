f4d2bd116c2161414a4a78b2eddbd521a1dc3f18
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-11-29 22:54:50 -0300

    Remove String Space - Challenge Finished.

--- script.js
+++ script.js
@@ -621,3 +621,28 @@ function opposite(number) {
 }
 
 console.log(opposite(-50));
+
+/* 18. REMOVE STRING SPACE
+_________________________________________________________________
+Simple, remove the spaces from the string, then return the resultant string.
+
+Sample tests:
+Test.describe("Example tests",_=>{
+Test.assertEquals(noSpace('8 j 8   mBliB8g  imjB8B8  jl  B'), '8j8mBliB8gimjB8B8jlB');
+Test.assertEquals(noSpace('8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd'), '88Bifk8hB8BB8BBBB888chl8BhBfd'); 
+Test.assertEquals(noSpace('8aaaaa dddd r     '), '8aaaaaddddr'); 
+});
+*/
+
+/* RESOLUTION:
+  It's a simple challenge, but made me research about the necessary solution,
+  that envolves Regex (that was the very first time that I coded a Regex solution). 
+*/
+
+function noSpace(x){
+ return x.replace(/\s/g, '');
+}
+
+console.log(noSpace("Re mo va  tod os os es pa ços ."));
+
+
