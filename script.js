469b32e4a8b33ebfcca927a73a2b91fb58639e40
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2020-12-01 12:31:06 -0300

    Beginner Series #1 School Pamerowrk - Challenge Finished.

--- script.js
+++ script.js
@@ -674,3 +674,31 @@ function positiveSum (arr) {
 }
 
 console.log(positiveSum()); //20
+
+
+/* BEGINNER SERIES #1 SCHOOL PAPAERWORK
+_________________________________________________________________
+
+Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+
+Your task is to calculate how many blank pages do you need.
+
+Example:
+paperwork(5, 5) == 25
+Note: if n < 0 or m < 0 return 0!
+*/
+
+/* RESOLUTION:
+  To solve that challenge, was necessary use a if/else composed with a verification if the parameters n and m was 
+  greater than 0. 
+  In case that n and/or m was smaller than 0, it must return 0.  
+*/
+
+paperwork = (n, m) => {
+  if (n > 0 && m > 0) {
+    return n * m
+  } else { 
+    return 0
+  }
+}
+console.log(paperwork(2, -5));
