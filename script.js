4f9d657bc5989af0a84539e7e9536b831a4a4cc9
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-12 12:13:38 -0300

    One Line Task: Make Pizza

--- script.js
+++ script.js
@@ -747,4 +747,44 @@ function simpleMultiplication(number) {
     return number * 9
   }
 }
-console.log(simpleMultiplication(2));ß
+console.log(simpleMultiplication(2));
+
+/* 25. ONE LINE TASK: MAKE PIZZA
+_________________________________________________________________
+Task
+Fix the code to pass all the tests. Unfortunately, you can only modify ONE line of code :(
+
+Rules
+Usually, the changes you make are limited to one line. Please don't complain that you can't write your own code, because this is a bugfix kata. Also, don't complain that the initial code is too messy. That's why you need to do the task ;-)
+
+You can do the following operations:
+1 - Replace the existing characters.
+
+2 - Add the character at the end of the line.
+
+You should not Add a new line or Delete a line.
+The overall difference of the modified code is less than 5 characters.
+*/
+
+/* RESOLUTION:
+  This challenge was solved with just 2 characters (+1) to follow the rules above.
+*/
+
+function makePizza(pieces) { //Let's make n pieces of Pizza ;-)
+  var result = "";
+  while ( pieces-- ){
+    var needSteps = 5;
+    while ( needSteps-- ) result += make( needSteps );
+  }
+  return result;
+}
+function make(step){
+  switch (step+1){
+    case 5: return "P";
+    case 4: return "i";
+    case 3: 
+    case 2: return "z";
+    case 1: return "a";
+  }
+}
+console.log(makePizza(3));
