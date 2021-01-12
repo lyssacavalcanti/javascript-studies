8444947e131162ac401da4bf6195c081c8ce6d92
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-01-12 15:14:54 -0300

    Power - Challenge Finished

--- script.js
+++ script.js
@@ -788,3 +788,28 @@ function make(step){
   }
 }
 console.log(makePizza(3));
+
+/* 26. POWER
+_________________________________________________________________
+The goal is to create a function 'numberToPower(number, power)' that "raises" the number up to power (ie multiplies number by itself power times).
+
+Examples
+numberToPower(3,2)  // -> 9 ( = 3 * 3 )
+numberToPower(2,3)  // -> 8 ( = 2 * 2 * 2 )
+numberToPower(10,6) // -> 1000000
+Note: Math.pow and some other Math functions like eval() and ** are disabled.
+*/
+
+function numberToPower(number, power) {
+  let total = 1;
+  for (let i = 1; i <= power; i++) {
+    total = total * number;
+  }
+  return total;
+};
+
+console.log(numberToPower(3,2));
+console.log(numberToPower(2,3));
+console.log(numberToPower(10,6));
+console.log(numberToPower(4,10));
+console.log(numberToPower(3,9));
