fdd828f8687c379486b6c3fc4768c67bfcaa02a0
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-02-01 16:27:46 -0300

    Incorrect Division Method - Challenge Finished

--- script.js
+++ script.js
@@ -908,3 +908,30 @@ function bonusTime(salary, bonus) {
 
  console.log(bonusTime(10000,true));
  console.log(bonusTime(78,false));
+
+/* 32. INCORRECT DIVISION METHOD
+_________________________________________________________________
+This method, which is supposed to return the result of dividing its first argument by its second, 
+isn't always returning correct values. Fix it.
+
+const solve = (x, y) => x // y
+
+SAMPLE TESTS
+const {assert} = require("chai");
+
+it("example tests", function() {
+  assert.strictEqual(solve(2, 1), 2);
+  assert.strictEqual(solve(2, 4), 0.5);
+  assert.strictEqual(solve(42, 0), Infinity);
+  assert.strictEqual(solve(0, 8), 0);
+  assert.strictEqual(solve(7, 2), 3.5);
+  assert.strictEqual(solve(4.2, 1), 4.2);
+  assert.strictEqual(solve(9999, 11), 909);
+});
+*/ 
+
+ const solve = (x, y) => {
+  return x / y;
+ };
+
+ console.log(solve(10,2));
