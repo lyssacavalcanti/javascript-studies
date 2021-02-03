19d035d4c84ebaed71302b32c739e560cb181552
lyssacavalcanti <lyssa.cavalcanti@gmail.com>
2021-02-03 14:21:52 -0300

    Sum The Strings - Challenge Finished

--- script.js
+++ script.js
@@ -964,7 +964,26 @@ Test.assertSimilar(fixTheMeerkat(["ground", "rainbow", "sky"]), ["sky", "rainbow
 function fixTheMeerkat(arr) {
   return arr.reverse()
  }
- 
+
  let arr = ["bottom", "middle", "top"]
 
  console.log(fixTheMeerkat(arr));
+
+ /* 34. SUM THE STRINGS
+_________________________________________________________________
+Create a function that takes 2 positive integers in form of a string as an input, and outputs the sum (also as a string):
+
+  sumStr("4", "5")    // should output "9"
+  sumStr("34", "5")   // should output "39"
+If either input is an empty string, consider it as zero.
+
+SAMPLE TESTS:
+Test.assertEquals(sumStr("4","5"), "9");
+Test.assertEquals(sumStr("34","5"), "39");
+*/ 
+
+function sumStr(a,b) {
+  return String(Number(a)+Number(b));
+}
+
+console.log(sumStr('0','5'));
